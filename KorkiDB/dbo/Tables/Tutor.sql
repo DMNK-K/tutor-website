b30b894dc13ed70d8c6c99a9824ca55ab39e863b
DMNK-K <76963592+DMNK-K@users.noreply.github.com>
2021-11-23 23:41:04 +0100

    Add begining of data access and db

---
+++ KorkiDB/dbo/Tables/Tutor.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Tutor]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [UserID] INT NOT NULL, 
+    [NameFirst] NVARCHAR(50) NOT NULL, 
+    [NameLast] NVARCHAR(50) NOT NULL, 
+    [NameFormal] NVARCHAR(50) NULL, 
+    [UsesFormalName] BIT NOT NULL DEFAULT 0, 
+    [GoesToClient] BIT NOT NULL DEFAULT 1, 
+    [Description] NVARCHAR(1250) NULL, 
+    [RatingCount] INT NOT NULL DEFAULT 0, 
+    [RatingSum] INT NOT NULL DEFAULT 0, 
+    [InfoPrice] NVARCHAR(250) NULL, 
+    [InfoBonus] NVARCHAR(500) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [ServiceRange] TINYINT NOT NULL DEFAULT 0
+)
---
+++ KorkiDB/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [IsTutor] BIT NOT NULL, 
+    [Email] NVARCHAR(100) NOT NULL, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [PasswordHash] BINARY(32) NOT NULL, 
+    [JoinDateTime] DATETIMEOFFSET(4) NOT NULL 
+)
