cb49d61aea961e2f6233eab155d31c59f0a962e4
DMNK-K <76963592+DMNK-K@users.noreply.github.com>
2021-11-24 00:40:38 +0100

    Add table for user ratings

---
+++ KorkiDB/dbo/Tables/Ratings.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Ratings]
+(
+	[GiverUID] INT NOT NULL PRIMARY KEY, 
+    [ReceiverUID] INT NOT NULL, 
+    [Rating] TINYINT NOT NULL
+)
--- KorkiDB/dbo/Tables/User.sql
+++ KorkiDB/dbo/Tables/User.sql
@@ -5,5 +5,5 @@
     [Email] NVARCHAR(100) NOT NULL, 
     [Username] NVARCHAR(50) NOT NULL, 
     [PasswordHash] BINARY(32) NOT NULL, 
-    [JoinDateTime] DATETIMEOFFSET(4) NOT NULL 
+    [JoinDateTime] DATETIMEOFFSET(3) NOT NULL 
 )
